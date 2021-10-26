d823fb873527d80efabd3758f65d620f5461cc3d
Apple <apple@Apples-MacBook-Air.local>
2021-10-26 23:18:52 +0530

    Updated forecast test

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)   
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+ '. It is ' + body.current.temperature + ' degree here. It feels like ' + body.current.feelslike + ' degree.')
+            callback(undefined, body.current.weather_descriptions[0]+ '. It is ' + body.current.temperature + ' degree outside. It feels like ' + body.current.feelslike + ' degree. The humidity is ' + body.current.humidity + '%.' )
 
         }
         
